002b3b86a16f8f8bef0c989720b54824549fb6da
mika <mika@nothing.org>
2018-04-13 14:54:35 +0800

    use mark strategy

--- scripts/color-date/color-date.user.js
+++ scripts/color-date/color-date.user.js
@@ -2,14 +2,52 @@
 // @name     colorDate
 // @namespace https://github.com/mika-cn/user-scripts
 // @description "Set date color according to date 根据网页上日期的新旧程度， 给日期进行着色， 比如说已经是5年前的一个日期会成为红色， 以便提醒阅览者，注意信息可能过于陈旧。"
-// @version  1.1.2
+// @version  1.1.3
 // @grant    none
 // @include *
 // @author   mika
 // ==/UserScript==
+/**
+ *
+ * # CHANGE LOG
+ * 2018-04-13 修改替换方式，可应对多个正则存在包含关系的情况
+ *
+ */
 
 (function(){
   'use strict';
+
+  // 主函数
+  function colorDate(){
+    var nodes = getMatchNode();
+    nodes.forEach(function(node){
+      var newNode = document.createElement("datetext");
+      newNode.innerHTML = applyRules(node.nodeValue, rules)
+      // replace child node
+      node.parentNode.replaceChild(newNode, node);
+    });
+  }
+
+
+  /*
+   * 根据规则(判断是否是日期)，获取满足的节点
+   */
+  function getMatchNode(){
+    var nodes = [];
+    var currentNode;
+    var nodeIterator = getTextNodeIterator();
+    while (currentNode = nodeIterator.nextNode()) {
+      var v = currentNode.nodeValue;
+      var rule = rules.some(function(rule){
+        return !!v.match(rule.regExp);
+      });
+      if(rule){
+        nodes.push(currentNode);
+      }
+    }
+    return nodes;
+  }
+
 
   /*
    * 获取文本节点迭代器
@@ -28,25 +66,6 @@
   }
 
 
-  /*
-   * 根据规则(判断是否是日期)，获取满足的节点
-   */
-  function getMatchNode(){
-    var nodes = [];
-    var currentNode;
-    var nodeIterator = getTextNodeIterator();
-    while (currentNode = nodeIterator.nextNode()) {
-      var v = currentNode.nodeValue;
-      var rule = rules.some(function(rule){
-        return !!v.match(rule.regExp);
-      });
-
-      if(rule){
-        nodes.push(currentNode);
-      }
-    }
-    return nodes;
-  }
 
   /*
    * 时长阀值与色值
@@ -78,25 +97,45 @@
   `).replace(/\n\s*/gm, '');
 
   /*
-   * 判断规则
+   * 判断规则(信息越详细，放在前面)
    */
   var rules = [
+    // yyyy-mm-dd
     {key: "01", regExp: /\d{4}-[01]?\d-[0-3]?\d/mg},
+    // yyyy/mm/dd
     {key: "01", regExp: /\d{4}\/[01]?\d\/[0-3]?\d/mg},
-    {key: "02", regExp: /\d{4}-[01]{1}\d(?!-\d[0-3]?\d)/mg},
-    {key: "02", regExp: /\d{4}\/[01]{1}\d(?!\/\d[0-3]?\d)/mg},
-    {key: "03", regExp: /(?:\s|^)[01]{1}\d-[0-3]{1}\d/mg},
+    // yyyy.mm.dd
+    {key: "01", regExp: /\d{4}\.[01]?\d\.[0-3]?\d/mg},
+    // month dd, yyyy
     {key: "01", regExp: new RegExp("(?:"+ monthPart +") [0-3]?\\d[,\\s]{1}\\s?\\d{4}", 'igm')},
+    // yyyy年mm月dd日
     {key: "01", regExp: /\d{4}年[01]?\d月[0-3]?\d日/mg},
-    {key: "02", regExp: /\d{4}年[01]?\d月(?![0-3]?\d日)/mg},
-    {key: "03", regExp: /(?:\s|^)[01]?\d月[0-3]?\d日/mg},
+    // yyyy-mm
+    {key: "02", regExp: /\d{4}-[01]{1}\d/mg},
+    // yyyy/mm
+    {key: "02", regExp: /\d{4}\/[01]{1}\d/mg},
+    // yyyy年mm月
+    {key: "02", regExp: /\d{4}年[01]?\d月/mg},
+    // mm-dd
+    {key: "03", regExp: /[01]{1}\d-[0-3]{1}\d/mg},
+    // mm月dd日
+    {key: "03", regExp: /[01]?\d月[0-3]?\d日/mg},
+    // N 天前
     {key: "04", regExp: /\d+\s?天前/mg},
+    // N days ago
     {key: "04", regExp: /\d+\s?days?\sago/mg},
+    // N 月前
     {key: "05", regExp: /\d+\s?月前/mg},
+    // N months ago
     {key: "05", regExp: /\d+\s?months?\sago/mg},
-    {key: "06", regExp: new RegExp("(?:"+ monthPart +")['\\s]{1}[0-3]{1}\\d(?![,\\s]{1}\\s?\\d{4})", 'igm')},
+    // N 年前
     {key: "07", regExp: /\d+\s?年前/mg},
+    // N years ago
     {key: "07", regExp: /\d+\s?years?\sago/mg},
+    // month yyyy
+    {key: "08", regExp: new RegExp("(?:"+ monthPart +")['\\s]{1}\\d{4}", 'igm')},
+    // month dd
+    {key: "06", regExp: new RegExp("(?:"+ monthPart +")['\\s]{1}[0-3]{1}\\d", 'igm')},
   ];
 
   /*
@@ -111,13 +150,14 @@
       case "05" : return handler_05();
       case "06" : return handler_06();
       case "07" : return handler_07();
+      case "08" : return handler_08();
       default: return function(match){ return match;};
     }
   }
 
   function handler_01(){
     return function(match){
-      var dateStr = match.replace(/年|月/g, '-').replace("日", '');
+      var dateStr = match.replace(/年|月|\./g, '-').replace("日", '');
       return replace(match, dateStr);
     };
   }
@@ -164,6 +204,12 @@
     };
   }
 
+  function handler_08(){
+    return function(match){
+      var v = match.replace(/'/, ' ').split(' ').join(" 01 ");
+      return replace(match, v);
+    };
+  }
 
   /*
    * 处理函数, 根据时长阀值着色
@@ -185,25 +231,48 @@
     }
   }
 
-
-  // 主函数
-  function colorDate(){
-    var nodes = getMatchNode();
-    nodes.forEach(function(node){
-      // replace child node
-      var newNode = document.createElement("datetext");
-      var html = node.nodeValue;
-      rules.forEach(function(rule){
-        var match = html.match(rule.regExp);
-        if(match){
-          html = html.replace(rule.regExp, getHandler(rule.key));
-        }
-      });
-      newNode.innerHTML = html;
-      node.parentNode.replaceChild(newNode, node);
-    });
+  function applyRules(text, rules){
+    var r = matchRules(text, rules);
+    return replaceMark(r.markedText, r.values);
   }
 
+  /*
+   * 根据rules，进行匹配，标记匹配, 并计算出替换值
+   * 用来避免两个正则是包含关系的情况
+   */
+  function matchRules(text, rules){
+    var idxUtil = makeIdxUtil(-1);
+    var values = [];
+    rules.forEach(function(rule){
+      var handler = getHandler(rule.key);
+      text = text.replace(rule.regExp, function(match){
+        values.push(handler(match));
+        var mark = "@[["+ idxUtil.next() +"]]";
+        return mark
+      });
+    });
+    return {
+      markedText: text,
+      values: values
+    }
+  }
+
+  // 索引工具
+  function makeIdxUtil(init){
+    return {
+      curr: init,
+      next: function(){ this.curr++; return this.curr; },
+    }
+  }
+
+
+  // 把标记替换为着色后的人日期
+  function replaceMark(text, values){
+    return text.replace(/@\[\[\d+\]\]/mg, function(mark){
+      var idx = parseInt(mark.match(/\d+/));
+      return values[idx]
+    });
+  }
 
 
   /*
